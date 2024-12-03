2029a2d87e4c8ec0c5e4197139568107bb768c31
Michael Kokadii <misha2479k@gmail.com>
2024-12-03 17:04:31 +0200

    Year 2024 Day 3

---
+++ years/2024/day3/index.js
@@ -0,0 +1,37 @@
+import { sumAll } from "../../../utils.js"
+
+function findMultiplications(input) {
+  const allMatches = input.matchAll(/mul\(\d{1,3},\d{1,3}\)/g)
+  return [...allMatches]
+}
+
+export function part1(input) {
+  const mulOperations = findMultiplications(input)
+
+  const multiplications = mulOperations.map(([match]) => {
+    const [first, second] = match.match(/\d{1,3}/g).map(Number)
+    return first * second
+  })
+
+  return sumAll(multiplications)
+}
+
+export function part2(input) {
+  const rows = input.split('don\'t()')
+
+  const matches = rows.slice(1).map(m => {
+    const [, ...rest] = m.split('do()')
+    return rest.join('')
+  })
+  matches.unshift(rows[0])
+
+  const mulOperations = matches.map(findMultiplications).flat()
+  // mulOperations.forEach((m, i) => console.log(i,m[0], m.index))
+
+  const multiplications = mulOperations.map(([match]) => {
+    const [first, second] = match.match(/\d{1,3}/g).map(Number)
+    return first * second
+  })
+
+  return sumAll(multiplications)
+}
