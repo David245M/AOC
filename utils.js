11c7f9a0a2d526e1f82cc3397585870f3e7206a5
Michael Kokadii <misha2479k@gmail.com>
2024-12-04 13:30:16 +0200

    Add transpose to utils

--- utils.js
+++ utils.js
@@ -1,2 +1,6 @@
+import { unzip } from "lodash-es";
+
 export const sumAll = arr => arr.reduce((a, b) => a + b, 0);
-export const mulAll = arr => arr.reduce((a, b) => a * b, 1); 
+export const mulAll = arr => arr.reduce((a, b) => a * b, 1);
+
+export const transpose = (matrix) => unzip(matrix)
