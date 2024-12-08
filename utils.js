f1ab4cf32dcf14e19b6417751c8b5e69e7973190
Михайло Кокадій <mikhailokokadii@MacBook-Air-Ganna.local>
2024-12-08 19:09:21 +0200

    Add in matrix util

--- utils.js
+++ utils.js
@@ -4,3 +4,9 @@ export const sumAll = arr => arr.reduce((a, b) => a + b, 0);
 export const mulAll = arr => arr.reduce((a, b) => a * b, 1);
 
 export const transpose = (matrix) => unzip(matrix)
+
+export const isInMatrix = (matrix, [y, x]) => {
+  return y >= 0 && x >= 0 &&
+    y < matrix.length &&
+    x < matrix[0].length
+}
