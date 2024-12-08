3b1b970dbe64742db8fd82e387fef300369d5075
Михайло Кокадій <mikhailokokadii@MacBook-Air-Ganna.local>
2024-12-08 19:08:53 +0200

    Year 2024 Day 7

---
+++ years/2024/day7/index.js
@@ -0,0 +1,70 @@
+import { sumAll } from "../../../utils.js"
+
+function formatInput(input) {
+  return input.split('\n').map((row) => {
+    const [left, right] = row.split(': ')
+    return [+left, right.split(' ').map(Number)]
+  })
+}
+
+function generateSignsSombination(signs, size) {
+  if (size === 1) {
+    return signs
+  }
+  return signs.map((sign) => {
+    const combinations = generateSignsSombination(signs, size - 1)
+    return combinations.map((combination) => {
+      return [sign, combination].flat()
+    })
+  }).flat()
+}
+
+function createEquasion(numbers, signs) {
+  const equasion = [numbers[0]]
+  for (let i = 0; i < signs.length; i++) {
+    equasion.push(signs[i], numbers[i + 1])
+  }
+  return equasion
+}
+
+export function part1(input) {
+  const equasions = formatInput(input)
+  const signs = [['*'], ['+']]
+
+  const trueEquasions = equasions.filter((equasion) => {
+    const [result, numbers] = equasion
+
+    const signCombinations = generateSignsSombination(signs, numbers.length - 1)
+
+    const equasionSolutions = signCombinations.map((comb) => {
+      return comb.reduce((acc, sign, i) => {
+        const equasion = acc + sign + numbers[i + 1]
+        return eval(equasion)
+      }, numbers[0])
+    })
+    return equasionSolutions.includes(result)
+  })
+
+  return sumAll(trueEquasions.map(([result]) => result))
+}
+
+export function part2(input) {
+  const signs = [['*'], ['+'], ['||']]
+  const equasions = formatInput(input)
+
+  const trueEquasions = equasions.filter((equasion) => {
+    const [result, numbers] = equasion
+
+    const signCombinations = generateSignsSombination(signs, numbers.length - 1)
+
+    const equasionSolutions = signCombinations.map((comb) => {
+      return comb.reduce((acc, sign, i) => {
+        const equasion = acc + sign + numbers[i + 1]
+        return sign === '||' ? Number(''+ acc + numbers[i + 1]) : eval(equasion)
+      }, numbers[0])
+    })
+    return equasionSolutions.includes(result)
+  })
+
+  return sumAll(trueEquasions.map(([result]) => result))
+}
